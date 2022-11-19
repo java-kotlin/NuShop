31a1686c09217841a69d916f5942156f55275ed1
HoaDinh21 <hoadt.21.it@gmail.com>
2022-11-19 20:36:32 +0700

    them ket noi vao db oracle

--- src/main/java/com/nu/config/DatabaseConfig.java
+++ src/main/java/com/nu/config/DatabaseConfig.java
@@ -1,44 +1,50 @@
-//package com.nu.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.jdbc.DataSourceBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import javax.sql.DataSource;
-//
-//@Configuration
-//public class DatabaseConfig {
-//
-////    @Value("${spring.datasource.url}")
-////    private String url;
-////
-////    @Value("${spring.datasource.username}")
-////    private String username;
-////
-////    @Value("${spring.datasource.password}")
-////    private String password;
-////    @Value("${spring.datasource.driver-class-name}")
-////    private String driverClass;
-////
-////    @Bean
-////    public DataSource dataSource() { // goi vao db ipay
-////        return DataSourceBuilder
-////                .create()
-////                .username("root")
-////                .password("Ab123456")
-////                .url("jdbc:mysql://localhost:3306/world")
-////                .driverClassName("com.mysql.cj.jdbc.Driver")
-////                .build();
-////    }
-////    @Bean
-////    public DataSource dataSourcePRF() { // goi vao db profile
-////        return DataSourceBuilder
-////                .create()
-////                .username("root")
-////                .password("Ab123456")
-////                .url("jdbc:mysql://localhost:3306/world")
-////                .driverClassName("com.mysql.cj.jdbc.Driver")
-////                .build();
-////    }
-//}
+package com.nu.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClass;
+
+    @Bean
+    public DataSource dataSource() { // goi vao db ipay
+        try {
+            DataSource dataSource = DataSourceBuilder
+                    .create()
+                    .username(username)
+                    .password(password)
+                    .url(url)
+                    .driverClassName(driverClass)
+                    .build();
+            return dataSource;
+        }catch (Exception e){
+            System.out.println(e.getCause());
+        }
+        return null;
+    }
+//    @Bean
+//    public DataSource dataSourcePRF() { // goi vao db profile
+//        return DataSourceBuilder
+//                .create()
+//                .username("root")
+//                .password("Ab123456")
+//                .url("jdbc:mysql://localhost:3306/world")
+//                .driverClassName("com.mysql.cj.jdbc.Driver")
+//                .build();
+//    }
+}
